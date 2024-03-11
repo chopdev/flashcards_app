5d5d75c4ace7beba5b294b55b519dbe59c117d05
Taras Plavin <tpplavin@amazon.com>
2024-03-10 22:50:47 -0700

    add new word modal

--- SimpleDictionary/DictionaryScreen.js
+++ SimpleDictionary/DictionaryScreen.js
@@ -1,36 +1,156 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, Button, ScrollView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import axios from 'axios';
 import {parseToWords} from './parsers/jsonParser'
 import testData from './parsers/test_export.json'
+import Modal from "react-native-modal";
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   wordContainer: {
     marginBottom: 10,
   },
   engText: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   translationsText: {
     fontSize: 16,
-    fontStyle: 'italic'
+    fontStyle: 'italic',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+    padding: 5,
+    width: '100%'
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  addButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  modal: {
+    margin: 0,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: '100%',
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonsContainer: {
+    display: "flex",
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10
   },
 });
 
 const DictionaryScreen = () => {
   const [words, setWords] = useState([]);
+  const [newWord, setNewWord] = useState({
+    eng: '',
+    transcription: '',
+    translation: '',
+    examples: [],
+  });
+  const [proposedTranslation, setProposedTranslation] = useState('');
+  const [pictureAssociation, setPictureAssociation] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     fetchWords();
   }, []);
 
+  const addExample = () => {
+    if (newWord.examples.length < 4) {
+      setNewWord(prevState => ({
+        ...prevState,
+        examples: [...prevState.examples, ''],
+      }));
+    } else {
+      // disable + button
+    }
+  };
+
+  const handleExampleChange = (example, index) => {
+    const updatedExamples = [...newWord.examples];
+    updatedExamples[index] = example;
+    setNewWord(prevState => ({
+      ...prevState,
+      examples: updatedExamples,
+    }));
+  };
+
+  const handleModalVisible = (visible) => {
+    setModalVisible(visible)
+    if (!visible) {
+      setNewWord(prevState => ({
+        eng: '',
+        transcription: '',
+        translation: '',
+        examples: [],
+      }));
+    }  
+  };
+
+  const handleAddWord = async () => {
+    // Implement logic to add a new word to the dictionary
+    try {
+      // Make POST request to get proposed translation
+      // const translationResponse = await axios.post('https://url/proposed-translation', {
+      //   englishWord: newWord.eng,
+      // });
+      // setProposedTranslation(translationResponse.data.translation);
+
+      // Make another POST request to get picture association
+      // const pictureResponse = await axios.post('https://url/picture-association', {
+      //   englishWord: newWord.eng,
+      // });
+      // setPictureAssociation(pictureResponse.data.pictureUrl);
+
+      if (!newWord.eng || !newWord.translation) {
+        return;
+      }
+
+      // After fetching proposed translation and picture association, add the new word to the dictionary
+      const updatedWords = [...words, newWord];
+      setWords(updatedWords);
+      setNewWord({
+        eng: '',
+        transcription: '',
+        translation: '',
+        examples: [],
+      });
+      // Close the modal after adding the word
+      setModalVisible(false);
+    } catch (error) {
+      console.error('Error adding word:', error);
+    }
+  }; 
+
   const fetchWords = async () => {
     try {
       //const response = await axios.get('your-backend-url/dictionary');
-      //
 
-      console.log('start reading');
+      console.log('Getting words');
       const words = await parseToWords(testData);
       setWords(words);
 
@@ -39,21 +159,68 @@ const DictionaryScreen = () => {
     }
   };
 
-  const addWord = async () => {
-    // Implement logic to add a new word
-  };
-
+  console.log('Render DictionaryScreen. Modal visible: ' + modalVisible)
   return (
-    <ScrollView>
-      {words.map((word, index) => (
-        <View style={styles.wordContainer} key={index}>
-          <Text style={styles.engText}>{word.eng}</Text>
-          <Text style={styles.translationsText}>{word.translations}</Text>
-          {/* Add button to play pronunciation */}
-      </View>
-      ))}
-      <Button title="Add Word" onPress={addWord} />
-    </ScrollView>
+    <View style={styles.container}>
+      {!modalVisible && <ScrollView>
+        <Button title="Add Word" onPress={() => handleModalVisible(true)} />
+        {words.map((word, index) => (
+          <View style={styles.wordContainer} key={index}>
+            <Text style={styles.engText}>{word.eng}</Text>
+            <Text style={styles.translationsText}>{word.translations}</Text>
+            {/* Add button to play pronunciation */}
+        </View>
+        ))}
+      </ScrollView>}
+      <Modal
+        isVisible={modalVisible}
+        swipeDirection={['down']}
+        onSwipeComplete={() => handleModalVisible(false)}
+        //onBackdropPress={() => setModalVisible(false)}
+        style={styles.modal}
+        //backdropColor="light grey"
+        backdropOpacity={0.5}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.buttonsContainer}>
+              <Button title="Save" onPress={handleAddWord} />
+              <Button title="Cancel" onPress={() => handleModalVisible(false)} />
+            </View>
+            <TextInput
+              style={styles.input}
+              placeholder="English word"
+              value={newWord.eng}
+              onChangeText={(text) => setNewWord(prevState => ({ ...prevState, eng: text }))}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Translation"
+              value={newWord.translation}
+              onChangeText={(text) => setNewWord(prevState => ({ ...prevState, translation: text }))}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Transcription (optional)"
+              value={newWord.transcription}
+              onChangeText={(text) => setNewWord(prevState => ({ ...prevState, transcription: text }))}
+            />
+            {newWord.examples.map((example, index) => (
+              <TextInput
+                key={index}
+                style={styles.input}
+                placeholder={`Example ${index + 1}`}
+                value={example}
+                onChangeText={(text) => handleExampleChange(text, index)}
+              />
+            ))}
+            <TouchableOpacity style={styles.addButton} onPress={addExample}>
+              <Text style={styles.addButtonText}>+ New example</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 };
 
